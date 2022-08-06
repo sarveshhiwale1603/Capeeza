af6ccb0adf57c4b39d1c8b86b05b908c70b9b1ba
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-08-06 23:58:49 +0530

    changes

--- Registration.php
+++ Registration.php
@@ -306,6 +306,7 @@
     <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
     <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
     <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <!-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> -->
 
     <!-- Template Main JS File -->
     <script src="assets/js/main.js"></script>
