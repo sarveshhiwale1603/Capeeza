ed0aa2c6e1a32d6ba1e0f95a21f534ac5894e49c
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-08-07 20:58:44 +0530

    ss

--- careers.php
+++ careers.php
@@ -230,14 +230,14 @@ if(isset($_POST['button']) && isset($_FILES['attachment']))
 	$body .= $encoded_content; // Attaching the encoded file with email
     
              // Message 
-             $body = '<h2>Contact Request Submitted</h2>
+             $htmlContent .= '<h2>Contact Request Submitted</h2>
              <p><b>Name:</b> '.$sender_name.'</p>
              <p><b>Email:</b> '.$reply_to_email.'</p>
              <p><b>Phone:</b> '.$subject.'</p>
              <p><b>Salary:</b> '.$Salary.'</p>
              <p><b>Message:</b><br/>'.$message.'</p>';
 	
-	$sentMailResult = mail($recipient_email, $subject, $body, $headers);
+	$sentMailResult = mail($recipient_email, $subject, $body, $htmlContent, $headers);
 
 	if($sentMailResult )
 	{
