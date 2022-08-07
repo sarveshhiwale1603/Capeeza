44cf04fc573e36035d004d7cf536361bf32e3370
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-08-08 00:02:50 +0530

    favicon

--- Business-Advisory.php
+++ Business-Advisory.php
@@ -10,7 +10,7 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="assets/images/logo/logo-capeeza.png" rel="icon">
+    <link href="assets/images/favicon.png" rel="icon">
     <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
