9b45c0372fe1924b788d96fb3250d8caa0dba453
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-08-08 11:40:47 +0530

    ss

--- careers.php
+++ careers.php
@@ -204,7 +204,6 @@ if(isset($_POST['button']) && isset($_FILES['attachment']))
 	$body .="Content-Transfer-Encoding: base64\r\n";
 	$body .="X-Attachment-Id: ".rand(1000, 99999)."\r\n\r\n";
 
-  echo "<script>console.log( " . $body . ");</script>";
 	$body .= $encoded_content; // Attaching the encoded file with email
     
 	
